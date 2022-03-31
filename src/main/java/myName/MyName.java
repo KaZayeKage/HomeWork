ac27c57af15b9520a5efbe9bd0bb0bad659fb3bf
Isaiah Holden <holdenisaiah4@gmail.com>
2022-03-31 12:49:26 -0400

    Adding hw

---
+++ src/main/java/myName/MyName.java
@@ -0,0 +1,162 @@
+package myName;
+
+import java.util.Arrays;
+
+public class MyName {
+
+
+
+
+
+
+//    Question 1
+//    public static int getLastIndex(String[] names){
+//
+//        return names.length - 1;
+
+//    }
+
+
+//      Question 2
+//    public static int getSecondToLastIndex(String[] names) {
+
+//        return names.length - 2;
+//    }
+
+
+    // Question 3
+//    public static String getFirstElement(String[] names) {
+//        return names[0];
+//    }
+
+   // Question 4
+//    public static String getLastElement(String[] names) {
+//        return names[4];
+//    }
+
+//Question 5
+//     public static String getSecondToLastElement(String[] names) {
+//      return names[5];
+//     }
+
+//    // Question 6
+//    public static int getSum(int[] ints) {
+//
+//        int totalSum = 0;
+//
+//    for (int x = 0; x < ints.length; x++){
+//        totalSum = totalSum  + ints[x];
+//    }
+//
+//        return totalSum;
+//    }
+
+//    // Question 7
+//    public static int getAverage(int[] ints) {
+//
+//        int totalSum = 0;
+//        for (int x = 0; x < ints.length; x++) {
+//            totalSum = totalSum + ints[x];
+//
+//        }
+//
+//        return totalSum / ints.length;
+//    }
+
+    // Question 8
+//    public static String extractAllOddNumbers(int[] numbers) {
+//
+//        for (int num = 0; num < numbers.length; num++) {
+//            if (numbers[num] % 2 == 1) {
+//                System.out.println(numbers[num]);
+//            }
+//        }
+//
+//
+//        return null;
+//
+//    }
+
+    // Question 9
+//    public static String extractAllEvenNumbers(int[] numbers) {
+//
+//        for(int x = 0; x < numbers.length; x++){
+//            if(numbers[x] % 2 == 0){
+//                System.out.println(numbers[x]);
+//            }
+//        }
+//
+//        return null;
+//    }
+
+    // Question 10
+//    public static boolean contains(String[] names, String element) {
+//      for (int x = 0; names.length > x; x++){
+//          if(names[x].equals(element)){
+//              return true;
+//          }
+//      }
+//        return false;
+//    }
+
+    public static void main(String[] args) {
+
+
+
+
+//          Question 1
+//        String[] ourNames = {"Mei", "Yu", "Ursa", "Wei", "Tay"};
+//        System.out.println(getLastIndex(ourNames));
+
+//        Question 2
+//        String[] newNames = {"Xei", "Nyx", "Wong", "Stephen", "Peter", "Otto"};
+//        System.out.println(getSecondToLastIndex(newNames));
+
+//         Question 3
+//        String[] firstNames = {"I", "Me", "You", "He", "She", "They"};
+//        System.out.println(getFirstElement(firstNames));
+
+//        //Question 4
+//        String[] firstNames = {"I", "Me", "You", "He", "She", "They"};
+//        System.out.println(getLastElement(firstNames));
+
+//        //Question 5
+//        String[] lastNames = {"I", "Me", "You", "He", "She", "They"};
+//        System.out.println(getSecondToLastElement(lastNames));
+
+//          //Question 6
+//        int[] myArray = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 20, 40};
+//        System.out.println(getSum(myArray));
+
+//            //Question 7
+//       int[] myArray = {1, 2, 3, 4};
+//        System.out.println(getAverage(myArray));
+
+            //Question 8
+//        int[] numbers = {1, 2, 3, 4, 5, 6, 7, 8, 9, 0};
+//        extractAllOddNumbers(numbers);
+
+        //Question 9
+//        int[] evenNumOnly = {10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0};
+//        extractAllEvenNumbers(evenNumOnly);
+
+//        String[] lastNames = {"Riordan", "Tolkien", "Rolkien", "Jolkien"};
+//        System.out.println(getLastElement(lastNames));
+
+//        Question 10
+//        String[] names = {"Isaiah", "John", "Luis", "Emoni"};
+//        System.out.println(contains(names, "Luis"));
+//
+//    }
+
+
+
+
+
+
+
+
+
+
+    }
+}
